e9f2501c202bdc4d1f4488027ca80a2efa73f595
Mohamed Alaa <malaaosmanosman5260@gmail.com>
2021-09-07 16:29:22 +0200

    test

--- src/components/Globals/Navbar.js
+++ src/components/Globals/Navbar.js
@@ -1,19 +1,34 @@
-import React, { Component } from 'react';
-import { Link } from "gatsby";
-import logo from '../../images/logo.svg';
-import { FaCartArrowDown } from 'react-icons/fa';
+import React, { Component } from "react"
+import { Link } from "gatsby"
+import logo from "../../images/logo.svg"
+import { FaCartArrowDown } from "react-icons/fa"
 export default class Navbar extends Component {
-
-    state={
-        navbarOpen: false,
-        css:'collapse navbar-collapse'
-    }
-
-    render() {
-        return (
-            <div>
-                navbar content
-            </div>
-        )
-    }
+  state = {
+    navbarOpen: false,
+    css: "collapse navbar-collapse",
+    links: [
+      {
+        id: 1,
+        path: "/",
+        text: "home",
+      },
+      {
+        id: 2,
+        path: "/about",
+        text: "about",
+      },
+    ],
+  }
+  navbarHandler = () => {
+    console.log("hello from handler navbar")
+  }
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm bg-light navbar-light">
+        <Link to="/" className="navbar-brand">
+          <img src={logo} alt="logo"/>
+        </Link>
+      </nav>
+    )
+  }
 }
