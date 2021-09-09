1f0b732a322f956024d48dcafa8b867ad6656118
Mohamed Alaa <malaaosmanosman5260@gmail.com>
2021-09-09 12:38:56 +0200

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,29 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import {FaGulp} from 'react-icons/fa'
-const IndexPage = () => (
+import BackgroundSection from "../components/Globals/BackgroundSection"
+const IndexPage = ({ data }) => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <FaGulp />
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="regular Joe's"
+      styleClass="default-background"
+    />
   </Layout>
-)
+);
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "default-background.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
