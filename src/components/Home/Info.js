623a0b67a6af02a65dab144d7b51f853810cdffe
Mohamed Alaa <malaaosmanosman5260@gmail.com>
2021-09-13 12:50:56 +0200

    main

--- src/components/Home/Info.js
+++ src/components/Home/Info.js
@@ -1,27 +1,26 @@
 import React from "react"
-import { Link } from "gatsby";
-import Title from '../Globals/Title';
+import { Link } from "gatsby"
+import Title from "../Globals/Title"
 export default function info() {
   return (
     <section className="py-5">
       <div className="container">
-          <Title title="our story"/>
+        <Title title="our story" />
         <div className="row">
           <div className="col-10 col-sm-8 mx-auto text-center">
             <p className="text-muted lead mb-5">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
+              Coffee is a brewed drink prepared from roasted coffee beans, the
+              seeds of berries from certain Coffea species. From the coffee
+              fruit, the seeds are separated to produce a stable, raw product:
+              unroasted green coffee. The seeds are then roasted, a process
+              which transforms them into a consumable product: roasted coffee,
+              which is ground into a powder and typically steeped in hot water
+              before being filtered out, producing a cup of coffee.
             </p>
             <Link to="/about">
-                <button className="btn text-uppercase btn-yellow">about page</button>
+              <button className="btn text-uppercase btn-yellow">
+                about page
+              </button>
             </Link>
           </div>
         </div>
--- src/pages/about.js
+++ src/pages/about.js
@@ -4,13 +4,13 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import BackgroundSection from "../components/Globals/BackgroundSection"
 import  Info from "../components/Home/Info";
-const IndexPage = ({ data }) => (
+const AboutPage = ({ data }) => (
   <Layout>
     <Seo title="Home" />
     <BackgroundSection
       img={data.img.childImageSharp.fluid}
-      title="regular Joe's"
-      styleClass="default-background"
+      title="about us"
+      styleClass="about-background"
     />
     <Info />
   </Layout>
@@ -18,7 +18,7 @@ const IndexPage = ({ data }) => (
 
 export const query = graphql`
   {
-    img: file(relativePath: { eq: "default-background.jpeg" }) {
+    img: file(relativePath: { eq: "about-background.jpeg" }) {
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid_tracedSVG
@@ -28,4 +28,4 @@ export const query = graphql`
   }
 `
 
-export default IndexPage
+export default AboutPage
