037ddd69dfd02e15a0f40658f15a045d9a9a907d
malaa5260 <malaaosmanosman5260@gmail.com>
2021-09-20 11:50:10 +0200

    test20_3

--- src/components/Home/product.js
+++ src/components/Home/product.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Product({ product }) {
   return (
-    <div className="col-lg-4 col-sm-8 col-10 col-md-6 mx-auto my-3 text-center">
+    <div className="col-lg-4 col-sm-8 col-md-6 mx-auto my-3 text-center">
       <img
         src={product.image.fluid.src}
         alt="coffeeitem"
