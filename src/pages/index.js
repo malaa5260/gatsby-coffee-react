c9f3a09a3727db21c9e34f4ba0a1822a02216de4
malaa5260 <malaaosmanosman5260@gmail.com>
2021-09-20 09:46:22 +0200

    day20

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import Seo from "../components/seo"
 import BackgroundSection from "../components/Globals/BackgroundSection"
 import  Info from "../components/Home/Info";
 import Menu from "../components/Home/Menu";
+import Products from "../components/Home/products";
+
 const IndexPage = ({ data }) => (
   <Layout>
     <Seo title="Home" />
@@ -15,6 +17,7 @@ const IndexPage = ({ data }) => (
     />
     <Info />
     <Menu items={data.menu} />
+    <Products/>
   </Layout>
 );
 
@@ -40,6 +43,20 @@ export const query = graphql`
           image {
             fixed(width: 75, height: 75) {
               ...GatsbyContentfulFixed_tracedSVG
+            }
+          }
+        }
+      }
+    }
+    products: allContentfulCoffeeProduct {
+      edges {
+        node {
+          id
+          title
+          price
+          image {
+            fluid(maxHeight:426) {
+              src
             }
           }
         }
