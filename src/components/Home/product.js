b48042f11d131e5ae6fd867e02d11360363713a1
malaa5260 <malaaosmanosman5260@gmail.com>
2021-09-25 14:04:54 +0200

    te

--- src/components/Home/product.js
+++ src/components/Home/product.js
@@ -17,7 +17,7 @@ export default function Product({ product }) {
             data-item-name={product.title}
             data-item-price={product.price}
             data-item-image={product.image.fluid.src}
-            data-item-url="https://gatsby-coffee-react.netlify.app/"
+            data-item-url="https://gatsby-coffee-react.netlify.app.json"
           >
             add to cart
           </button>
