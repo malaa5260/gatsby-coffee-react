de9c43b7e96d6ce9a45264cd92e4ea6413e12a7b
malaa5260 <malaaosmanosman5260@gmail.com>
2021-09-25 12:16:03 +0200

    snipcart

--- src/components/Home/product.js
+++ src/components/Home/product.js
@@ -16,8 +16,8 @@ export default function Product({ product }) {
             data-item-id={product.id}
             data-item-name={product.title}
             data-item-price={product.price}
-            data-item-url="https://gatsby-coffee-react.netlify.app/"
             data-item-image={product.image.fluid.src}
+            data-item-url="https://gatsby-coffee-react.netlify.app/"
           >
             add to cart
           </button>
